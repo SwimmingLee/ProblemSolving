70b7ae8b05cdcb52910ee55a390bd43ed8a20d46
SwimmingLee <swimming_7291@daum.net>
2021-05-18 23:56:52 +0900

    dfs 코드

--- (Pyton)ProblemSolving/ProblemSovling/배열/[LeetCode] 배열.py
+++ (Pyton)ProblemSolving/ProblemSovling/배열/[LeetCode] 배열.py
@@ -1,1 +1,29 @@
-배열 구하기
+# 배열 구하기
+
+import sys
+
+sys.setrecursionlimit(10001)
+
+
+def solution(value, project):
+    N = len(value)
+    adj = [[] for _ in range(N)]
+    for edge in project:
+        u, v = edge
+        adj[u-1].append(v-1)
+
+    cache = [-1] * N
+
+    def dfs(node):
+        child_value = 0
+
+        for next_node in adj[node]:
+            child_value = max(child_value, dfs(next_node))
+
+        return child_value + value[node]
+
+    return dfs(0)
+
+
+print(solution([10, 11, 8, 4, 9, 15, 17], [
+      [1, 2], [1, 3], [1, 4], [3, 5], [3, 6], [4, 7]]))
