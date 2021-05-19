4bba7b465bc4b4330dde89e9cffa8b7bea9e9c9b
SwimmingLee <swimming_7291@daum.net>
2021-05-19 23:45:59 +0900

    vector max_size 실험

--- (C++)ProblemSolving/ProblemSolving/overflow_tset.cpp
+++ (C++)ProblemSolving/ProblemSolving/overflow_tset.cpp
@@ -1,10 +1,27 @@
-//#include <iostream>
-//using namespace std;
-//
-//int main(void) {
-//	int a = 1 << 31;
-//	for (int i = 0; i < 10; i++) {
-//		cout << a - i << endl;
-//	}
-//	return 0;
-//}
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct drop {
+	int cell_idx;
+	int cell_pixel_x;
+	int cell_pixel_y;
+	int nozzle_idx;
+	double glass_x;
+	double glass_y;
+	double volume;
+};
+
+int main(void) {
+	vector<int> int_vector;
+	cout << "vector<int>.max_size :" << int_vector.max_size() << endl;
+	cout << "vector<int>.max_size * sizeof(int):" << int_vector.max_size() * sizeof(int) << endl;
+
+	vector<drop> drop_vector;
+	cout << "vector<drop>.max_size :" << drop_vector.max_size() << endl;
+	cout << "vector<drop>.max_size  * sizeof(drop):" << drop_vector.max_size() * sizeof(drop)<< endl;
+
+	cout << "UINT32_MAX : " << UINT32_MAX << endl;
+	cout << "UINT64_MAX : " << UINT64_MAX << endl;
+	return 0;
+}
