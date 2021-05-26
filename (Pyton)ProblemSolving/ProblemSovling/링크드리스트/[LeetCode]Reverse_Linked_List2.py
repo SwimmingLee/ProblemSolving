aba9d500bdf99ab5b87689c9d1fa90d3168ec1c4
SwimmingLee <swimming_7291@daum.net>
2021-05-26 10:24:08 +0900

    [LeetCode] 링크드 리스트 문제 풀이

--- (Pyton)ProblemSolving/ProblemSovling/링크드리스트/[LeetCode]Reverse_Linked_List2.py
+++ (Pyton)ProblemSolving/ProblemSovling/링크드리스트/[LeetCode]Reverse_Linked_List2.py
@@ -6,10 +6,16 @@
 class Solution:
     def reverseBetween(self, head: ListNode, left: int, right: int) -> ListNode:
 
-        start = head
+        dummy = ListNode(None)
+        dummy.next = head
+        start = dummy
         for _ in range(left-1):
             start = start.next
         end = start.next
+        tmp = start.next
 
         for _ in range(right - left):
-            end.next, end = end, end.next.next
+            start.next, end.next = end.next, end.next.next
+            tmp, tmp.next = start.next, tmp
+
+        return dummy.next
